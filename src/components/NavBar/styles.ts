1659219bef2cb0310d829a6693d1a96258855aca
Kauan <kauan.s.leite@hotmail.com>
2023-09-12 19:35:46 -0300

    feat(navbar): styled themes

--- src/components/NavBar/styles.ts
+++ src/components/NavBar/styles.ts
@@ -2,18 +2,21 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   top: 28%;
-  right: 2.6%;
+  right: 0%;
   position: fixed;
+  margin-right: 50px;
 `
 
 export const Nav = styled.div`
-  background-color: #FAB301;
+  background-color: ${props => props.theme.colors.secondary};
   display: flex;
   flex-direction: column;
   max-width: 40px;
   justify-content: center;
   align-items: center;
   border-radius: 40px;
+  transition: .5s;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
 
   a {
     text-decoration: none;
